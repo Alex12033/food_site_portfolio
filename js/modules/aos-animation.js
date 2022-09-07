efcad2a189d413a270fcdc3531ff1e63b7d6e0d7
Aleksandr <aleksandrignashov42@gmail.com>
2022-09-07 11:16:42 +0300

    was added script with options for animation

---
+++ js/modules/aos-animation.js
@@ -0,0 +1,11 @@
+function animate() {
+  AOS.init({
+    offset: 100,
+    duration: 400,
+    anchorPlacement: 'top-bottom',
+    easing: "ease-in-sine",
+    delay: 100,
+  });
+}
+
+export default animate;
